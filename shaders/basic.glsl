f86ba4200e16cd0155c042035a16fc704f4c32af
Kay Verbruggen <kay.verbruggen@gmail.com>
2018-02-12 12:32:42 +0100

    Add Directional light!

--- shaders/basic.glsl
+++ shaders/basic.glsl
@@ -46,24 +46,29 @@ uniform sampler2D texture_sampler;
 uniform Directional_Light dir_light;
 uniform vec3 view_pos;
 
-void main()
+vec4 calc_dir_light(Directional_Light dl)
 {
     // Ambient
     float ambient_strength = 0.1;
-    vec3 ambient = ambient_strength * dir_light.base.color;
+    vec3 ambient = ambient_strength * dl.base.color;
 
     // Diffuse
     vec4 diffuse_color = texture(texture_sampler, tex_coord);
     vec3 n = normalize(normal); 
-    float diff = max(dot(n, dir_light.direction), 0.0);
-    vec3 diffuse = diff * dir_light.base.color * dir_light.base.intensity;
+    float diff = max(dot(n, -dl.direction), 0.0);
+    vec3 diffuse = diff * dl.base.color * dl.base.intensity;
 
     // Specular
     float specular_strength = 0.3;
     vec3 view_dir = normalize(view_pos - position);
-    vec3 reflect_dir = reflect(-dir_light.direction, n);
+    vec3 reflect_dir = reflect(dl.direction, n);
     float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 32);
-    vec3 specular = specular_strength * spec * dir_light.base.color;
+    vec3 specular = specular_strength * spec * dl.base.color;
 
-    color = diffuse_color * vec4(ambient + diffuse + specular, 1.0);
+    return diffuse_color * vec4(ambient + diffuse + specular, 1.0);
+}
+
+void main()
+{
+    color = calc_dir_light(dir_light);
 }
